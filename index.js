a2bd8ab7daff2c13b3f41cd8e5280223a74728b8
hardspike <44533313+hardspike@users.noreply.github.com>
2019-05-02 18:07:56 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 "use strict";
 
-(function() {
-  for (let i = 0; i < 4; i++) {
-    document
-      .getElementsByClassName("thumb")
-      [i].addEventListener("mouseover", function(e) {
-        document.getElementsByClassName("preview")[i].style.left =
-          e.clientX + "px";
-        document.getElementsByClassName("preview")[i].style.top =
-          e.clientY + "px";
-        document.getElementsByClassName("preview")[i].style.display =
-          "inline-block";
-        document.getElementsByClassName("preview")[i].style.position =
-          "absolute !important";
-      });
+(function () {
+    for (let i = 0; i < 6; i++) {
+        document
+            .getElementsByClassName("thumb")
+        [i].addEventListener("mouseover", function (e) {
+            document.getElementsByClassName("preview")[i].style.left =
+                e.clientX + "px";
+            document.getElementsByClassName("preview")[i].style.top =
+                e.clientY + "px";
+            document.getElementsByClassName("preview")[i].style.display =
+                "inline-block";
+            document.getElementsByClassName("preview")[i].style.position =
+                "absolute !important";
+        });
 
-    document
-      .getElementsByClassName("thumb")
-      [i].addEventListener("mouseout", function() {
-        document.getElementsByClassName("preview")[i].style.display = "none";
-      });
-  }
+        document
+            .getElementsByClassName("thumb")
+        [i].addEventListener("mouseout", function () {
+            document.getElementsByClassName("preview")[i].style.display = "none";
+        });
+    }
+
+    $(".biu img").click(function () {
+        const $url = $(this).attr("name");
+        window.open($url, "_blank");
+    });
+    $(".biu a").click(function(e) {
+        e.preventDefault();
+    });
+
 })();
